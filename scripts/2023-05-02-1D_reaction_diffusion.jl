06c26e5e459c4f1fd77dcd741637b77bd7a86f4d
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-05-02 18:14:33 +0200

    Simplify the demo

--- scripts/2023-05-02-1D_reaction_diffusion.jl
+++ scripts/2023-05-02-1D_reaction_diffusion.jl
@@ -1,19 +1,18 @@
 #=
 A quick demo for how to use the new reaciton diffusion definitions by making some gifs
 =#
-using OrdinaryDiffEq, LinearAlgebra, ForwardDiff, SimpleUnPack, SparseArrays, ProbNumDiffEq
-using Plots
+using OrdinaryDiffEq, Plots
 import BayesExpIntExperiments as BEIE
 
 ############################################################################################
 # Fisher / Spruce-Budworm gif
 ############################################################################################
 prob, L = BEIE.prob_rd_1d_fisher(; N=32);
-sol_acc = solve(prob, RadauIIA5(), abstol=1e-10, reltol=1e-10, saveat=1);
+sol = solve(prob, RadauIIA5(), abstol=1e-10, reltol=1e-10, saveat=1);
 
 # Create and save the gif
-anim = @animate for t in 0:prob.tspan[2]
-    plot(sol_acc(t), ylim=(0, 1), label="", title="t = $t", ylabel="u(x, t)", xlabel="x")
+anim = @animate for i in 1:length(sol)
+    plot(sol[i], ylim=(0, 1), label="", title="t = $(sol.t[i])", ylabel="u(x, t)", xlabel="x")
 end
 gif(anim, "1dreactiondiffusion.gif", fps=15)
 
@@ -21,17 +20,24 @@ gif(anim, "1dreactiondiffusion.gif", fps=15)
 # SIR gif
 ############################################################################################
 prob, _ = BEIE.prob_rd_1d_sir(; N=64);
-sol_acc = solve(prob, Vern9(), abstol=1e-10, reltol=1e-10);
+sol = solve(prob, Vern9(), abstol=1e-10, reltol=1e-10, saveat=1);
 
 # Create and save the gif
-anim = @animate for t in 0:prob.tspan[2]
+anim = @animate for i in 1:length(sol)
     plot(
-        reshape(sol_acc(t), :, 3),
+        reshape(sol[i], :, 3),
         ylim=(0, prob.p.reaction!.P),
         label="",
-        title="t = $t",
+        title="t = $(sol.t[i])",
         ylabel="u(x, t)",
         xlabel="x",
     )
 end
 gif(anim, "1dreactiondiffusion.gif", fps=30)
+
+# Or as a heatmap:
+sol_arr = reshape(Array(sol), :, 3, length(sol))
+p1 = heatmap(sol_arr[:, 1, :], xticks=false, ylabel="x", title="S")
+p2 = heatmap(sol_arr[:, 2, :], xticks=false, ylabel="x", title="I")
+p3 = heatmap(sol_arr[:, 3, :], xlabel="t", ylabel="x", title="R")
+plot(p1, p2, p3, layout=(3, 1))
